f374fa3a1e079f2ffeb445f3fc2fb6ec3737475f
Shamsundar04 <chapakanadesham@gmail.com>
2026-01-31 22:38:56 +0530

    Array program added

--- src/in/array/ShiftingElements.java
+++ src/in/array/ShiftingElements.java
@@ -1,7 +1,5 @@
 package in.array;
 
-import java.util.Collections;
-
 public class ShiftingElements {
 
 	public static void main(String[] args) {
@@ -9,12 +7,33 @@ public class ShiftingElements {
 //		Input:  [0, 1, 0, 3, 12]
 //		Output: [1, 3, 12, 0, 0]
 		
+//		int a[]= {0,1,0,3,12};
+//		int index=0;
+//		
+//		for(int i=0;i<a.length;i++) {
+//			
+//			if (0!=a[i]) {
+//				a[index]=a[i];
+//				index++;
+//			}
+//		}
+//		
+//		for(int i=index;i<a.length;i++) {
+//			a[i]=0;
+//		}
+//		
+//		for (int i : a) {
+//			System.out.print(i+" ");
+//			
+//		}
+		
+		
 		int a[]= {0,1,0,3,12};
 		int index=0;
 		
 		for(int i=0;i<a.length;i++) {
 			
-			if (0!=a[i]) {
+			if (a[i]!=0) {
 				a[index]=a[i];
 				index++;
 			}
@@ -26,9 +45,6 @@ public class ShiftingElements {
 		
 		for (int i : a) {
 			System.out.print(i+" ");
-			
 		}
-		
-
 	}
 }
