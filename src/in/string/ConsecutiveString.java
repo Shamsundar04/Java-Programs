c1efae63017d9a07aa9e51f64e43b1a4f55abff4
Shamsundar04 <chapakanadesham@gmail.com>
2026-03-25 23:53:34 +0530

    updated

--- src/in/string/ConsecutiveString.java
+++ src/in/string/ConsecutiveString.java
@@ -4,49 +4,54 @@ public class ConsecutiveString {
 
 	public static void main(String[] args) {
 
-		String str = "aaabbccddd";
-		
-		for(int i=0;i<str.length();i++) {	
-			int count=1;
-			for(int j=i+1;j<str.length();j++) {
-				if (str.charAt(i)==str.charAt(j)) {
-					count++;
-				}
-			}
-			
-			System.out.print(str.charAt(i)+""+count);
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		StringBuilder result = new StringBuilder();
+	    String str = "aaabbccddd";
+        boolean[] visited = new boolean[str.length()];
 
-		int count = 1;
+        for (int i = 0; i < str.length(); i++) {
 
-		for (int i = 1; i <= str.length(); i++) {
+            if (visited[i]) continue;
 
-			if (i < str.length() && (str.charAt(i) == (str.charAt(i - 1)))) {
-				count++;
-			} else {
-				result.append(str.charAt(i-1)).append(count);
-				count=1;
-			}
-		}
-		System.out.println(result.toString());
+            int count = 1;
+
+            for (int j = i + 1; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+
+            System.out.print(str.charAt(i) + "" + count);
+        }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		StringBuilder result = new StringBuilder();
+//
+//		int count = 1;
+//
+//		for (int i = 1; i <= str.length(); i++) {
+//
+//			if (i < str.length() && (str.charAt(i) == (str.charAt(i - 1)))) {
+//				count++;
+//			} else {
+//				result.append(str.charAt(i-1)).append(count);
+//				count=1;
+//			}
+//		}
+//		System.out.println(result.toString());
 	}
 }
