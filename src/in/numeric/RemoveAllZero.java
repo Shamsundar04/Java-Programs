05b93ff51529fc78742561c3bc1cca10bef9556a
Shamsundar04 <chapakanadesham@gmail.com>
2026-03-26 16:02:51 +0530

    updated

--- src/in/numeric/RemoveAllZero.java
+++ src/in/numeric/RemoveAllZero.java
@@ -7,15 +7,19 @@ public class RemoveAllZero {
 		int num=10204;
 		StringBuilder remove=new StringBuilder(); 
 		
+		String replace = String.valueOf(num).replace("0", "");
+		int int1 = Integer.parseInt(replace);
 		
-		while (num!=0) {
-			
-			if (num%10!=0) {
-				remove.append(num%10);
-			}
-			num=num/10;
-		}
-		String string = remove.reverse().toString();
-		System.out.println(Integer.parseInt(string));
+		System.out.println(int1);
+		
+//		while (num!=0) {
+//			
+//			if (num%10!=0) {
+//				remove.append(num%10);
+//			}
+//			num=num/10;
+//		}
+//		String string = remove.reverse().toString();
+//		System.out.println(Integer.parseInt(string));
 	}
 }
