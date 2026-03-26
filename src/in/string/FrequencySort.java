3aad7b82205b6bf3a05e1869f86868ecfe835435
Shamsundar04 <chapakanadesham@gmail.com>
2026-03-26 13:53:46 +0530

    updated

--- src/in/string/FrequencySort.java
+++ src/in/string/FrequencySort.java
@@ -17,7 +17,7 @@ class FrequencySort {
         }
         
         List<Character> list = new ArrayList<>(map.keySet());
-        list.sort((a,b)-> map.get(b) - map.get(a));
+//        list.sort((a,b)-> map.get(b) - map.get(a));
 //        list.forEach(System.out::println);
         
         
@@ -29,7 +29,7 @@ class FrequencySort {
         
         for(int i=0;i<freq.length;i++){
             if(freq[i]>0){	
-//                System.out.print((char)i+" "+freq[i]);
+                System.out.print((char)i+""+freq[i]);
             }
         }
     }
