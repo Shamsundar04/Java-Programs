8f5bcf4eeb525e90381a68d0b4b70c6dde27e62e
Shamsundar04 <chapakanadesham@gmail.com>
2026-08-19 19:18:31 +0530

    String program firstrepeat

--- src/in/string/ReverseString.java
+++ src/in/string/ReverseString.java
@@ -3,18 +3,38 @@ package in.string;
 public class ReverseString {
 
 	public static void main(String[] args) {
-
-		String word = "Hello";
 		
-		String reverse="";
 		
-		for(int i=word.length()-1;i>=0;i--) {
-			reverse+=word.charAt(i);
+		int[] arr = {3, 1, 3, 4, 5};
+		//repeating and missing
+		
+		boolean visited[]=new boolean[arr.length];
+		
+		for (int i = 0; i < arr.length; i++) {
+			int count=1;
+			for (int j = i+1; j < arr.length; j++) {
+				if (arr[i]==arr[j]) {
+					count++;
+				}
+			}
+			
+			if (count>1) {
+				System.out.println(arr[i]);
+			}
 		}
+		
 
-		System.out.println(reverse);
-
-		String word1 = new StringBuilder(word).reverse().toString();
-		System.out.println(word1);
+//		String word = "Hello";
+//		
+//		String reverse="";
+//		
+//		for(int i=word.length()-1;i>=0;i--) {
+//			reverse+=word.charAt(i);
+//		}
+//
+//		System.out.println(reverse);
+//
+//		String word1 = new StringBuilder(word).reverse().toString();
+//		System.out.println(word1);
 	}
 }
