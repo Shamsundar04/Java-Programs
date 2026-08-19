545db8caeddf5fa9ecf5984a46270dec237bdbd2
Shamsundar04 <chapakanadesham@gmail.com>
2026-08-19 19:04:38 +0530

    String program firstrepeat

--- src/in/string/ReverseString.java
+++ src/in/string/ReverseString.java
@@ -6,13 +6,13 @@ public class ReverseString {
 
 		String word = "Hello";
 		
-//		String reverse="";
-//		
-//		for (int i = word.length()-1; i >= 0; i--) {
-//			reverse+=word.charAt(i);
-//		}
-//
-//		System.out.println(reverse);
+		String reverse="";
+		
+		for (int i = word.length()-1; i >= 0; i--) {
+			reverse+=word.charAt(i);
+		}
+
+		System.out.println(reverse);
 
 		String word1 = new StringBuilder(word).reverse().toString();
 		System.out.println(word1);
