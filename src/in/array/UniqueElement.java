31be07219c42db118aeb9ac22af935e62bd48cef
Shamsundar04 <chapakanadesham@gmail.com>
2026-08-22 22:51:44 +0530

    frequency

--- src/in/array/UniqueElement.java
+++ src/in/array/UniqueElement.java
@@ -1,25 +1,40 @@
 package in.array;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 public class UniqueElement {
 	public static void main(String[] args) {
 		
 		int arr[]= {1,2,1,3,4,6,2,5,4};
 		
 		
+		Map<Integer, Integer> freq=new HashMap<>();
 		
-		for(int i=0;i<arr.length;i++) {
-			int c=0;
-			for(int j=0;j<arr.length;j++) {
-				
-				if (arr[i]==arr[j]) {
-					c++;
-				}
-			}
-
-			if (c==1) {
-				System.out.println(arr[i]);
-			}			
+		for (int num : arr) {
+			freq.put(num, freq.getOrDefault(num, 0)+1);
 		}
+		
+		for(Map.Entry<Integer, Integer> entrySet : freq.entrySet()) {
+			System.out.println(entrySet.getKey()+" "+entrySet.getValue());
+		}
+		
+		
+//		for(int i=0;i<arr.length;i++) {
+//			int c=0;
+//			for(int j=0;j<arr.length;j++) {
+//				
+//				if (arr[i]==arr[j]) {
+//					c++;
+//				}
+//			}
+//
+//			if (c==1) {
+//				System.out.println(arr[i]);
+//			}			
+//		}
 		
 		
 //		3 6 5
